8cc0ad3ba855ade0abe946a4dd043764ca927716
luncheon <takeuck+github@gmail.com>
2026-03-21 17:17:06 +0900

    perf: use `toString(2)` instead of bit operators to get bigint binary

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const encodeBinary = (x, length) => {
   const a = [];
-  while (length--) a.push(x & (1 << length) ? 1 : 0)
-  return a
-}
-const encodeBigBinary = (x, length) => {
-  const a = [];
-  while (length--) a.push(x & (1n << length) ? 1 : 0)
-  return a
-}
+  while (length--) a.push(x & (1 << length) ? 1 : 0);
+  return a;
+};
+const encodeBigBinary = (x, length) =>
+  x
+    .toString(2)
+    .padStart(length, "0")
+    .split("")
+    .map((c) => parseInt(c));
 
 export const encodeTruncatedBinary = (x, upperBound) => {
   if (x < 0 || x >= upperBound) throw RangeError("encodeTruncatedBinary: x must be in range [0, upperBound).");
   if (upperBound > 0x3fffffff) {
     x = BigInt(x);
     upperBound = BigInt(upperBound);
-    const b = BigInt(upperBound.toString(2).length);
-    const u = (1n << b) - upperBound;
-    return x < u ? encodeBigBinary(x, b - 1n) : encodeBigBinary(x + u, b);
+    const b = upperBound.toString(2).length;
+    const u = (1n << BigInt(b)) - upperBound;
+    return x < u ? encodeBigBinary(x, b - 1) : encodeBigBinary(x + u, b);
   } else {
     const b = 32 - Math.clz32(upperBound);
     const u = (1 << b) - upperBound;
